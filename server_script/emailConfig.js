bf410cffa3761c865e8bb8567494d1d478addedd
groot737 <giorgiquchuloria7@gmail.com>
2023-09-27 02:59:40 +0400

    added emailconfig

--- server_script/emailConfig.js
+++ server_script/emailConfig.js
@@ -4,7 +4,6 @@ require('dotenv').config()
 const transporter = nodemailer.createTransport({
     host: process.env.HOST,
     port: 587,
-    secure: false,
     auth: {
         user: process.env.LOGIN,
         pass: process.env.PASS
